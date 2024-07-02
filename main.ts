e740cf64b66704ce4167adf20eaef6763bfe85a5
notlegos <roger@mustacich.com>
2024-07-01 20:30:14 -0700

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,12 @@
+function raiseAlarm (duration: number) {
+    Connected.ledBrightness(Connected.DigitalRJPin.J3, true)
+    basic.pause(duration)
+    Connected.ledBrightness(Connected.DigitalRJPin.J3, false)
+}
 let thisRead = 0
 Connected.laserSensor(Connected.DigitalRJPin.J4, true)
+pins.setAudioPinEnabled(false)
+Connected.oledClear()
 radio.setGroup(80)
 let breaks = 0
 let lastRead = pins.analogReadPin(AnalogPin.P2)
@@ -8,20 +15,13 @@ while (true) {
     thisRead = pins.analogReadPin(AnalogPin.P2)
     if (thisRead < 145) {
         breaks = breaks + 1
-        Connected.ledBrightness(Connected.DigitalRJPin.J3, true)
-        basic.pause(500)
+        raiseAlarm(500)
         Connected.showUserNumber(3, breaks)
         Connected.showUserNumber(4, lastRead)
         Connected.showUserNumber(5, thisRead)
-        Connected.ledBrightness(Connected.DigitalRJPin.J3, false)
     }
     lastRead = thisRead
 }
-pins.setAudioPinEnabled(false)
-Connected.oledClear()
-loops.everyInterval(2000, function () {
-	
-})
 basic.forever(function () {
 	
 })
